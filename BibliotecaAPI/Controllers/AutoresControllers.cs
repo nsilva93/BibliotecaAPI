efbefd9804bb427c87bed1648b5778771ceb56ba
NeriSilva9 <neri_15__5@hotmail.com>
2026-06-08 16:32:32 -0600

    Proyecto inicial BibliotecaAPI

---
+++ BibliotecaAPI/Controllers/AutoresControllers.cs
@@ -0,0 +1,68 @@
+﻿using BibliotecaAPI.Datos;
+using BibliotecaAPI.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresControllers : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresControllers(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Autor>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            var autor = await context.Autores
+                .Include(x => x.Libros)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (autor is null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Autor autor)
+        {
+            if (id != autor.Id)
+            {
+                return BadRequest("Los ids deben coincidir");
+            }
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var registrosBorrado = await context.Autores.Where(x => x.Id == id).ExecuteDeleteAsync();
+            if (registrosBorrado == 0)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+    }
+}
